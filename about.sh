e0a0a0f11d47afdfcdf18fe19bf8c80e4669c16a
aimanhakimka <93132605+aimanhakimka@users.noreply.github.com>
2021-10-28 12:12:59 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "===================================================="
+echo -e "      Script Auto Install SSH & OpenVPN v1.0        " 
+echo -e "													 "
+echo -e "          ### ### ##       #### ##                  "
+echo -e "           ## ##  #       ##  # ##                  "
+echo -e "           ## ## ##  ## # ###   ####   ###  ###     "
+echo -e "           ## ## #   ####  ###  ## #  ## ## ## #    "
+echo -e "            ## ###   ##     ### ## #  ## ## ## #    "
+echo -e "            ## ##    ##   #  ## ## #  ## ## ## #    "
+echo -e "             #  #    ##   ####  ## ##  ###  ###     "
+echo -e "                                            ##      "
+echo -e "                                            ###     "
+echo -e "===================================================="
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "Only for VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Wrshop"
+echo -e ""
+echo -e "Check My Shop At Shopee : wrshop0405"
+echo -e "Telegram                : https://t.me/WrShop0405"
+echo -e ""
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
