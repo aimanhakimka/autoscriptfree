db236c16e42cd7c28b5cde30ebf7b2afaf423f04
aimanhakimka <93132605+aimanhakimka@users.noreply.github.com>
2021-10-28 12:39:37 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Menu
+
+echo -e "================================MENU====================================="
+echo -e "* menu       : Senarai Menu"
+echo -e "* usernew    : Create akaun SSH"
+echo -e "* trial      : Membuat akaun trial"
+echo -e "* hapus      : Delete akaun SSH"
+echo -e "* login      : Check login user"
+echo -e "* member     : Senarai user"
+echo -e "* resvis     : restart service dropbear, webmin, squid3, openvpn dan ssh"
+echo -e "* reboot     : reboot VPS"
+echo -e "* speedtest  : speedtest VPS"
+echo -e "* info       : Info sistem"
+echo -e "* about      : Informasi Tentang script Auto install WrShop"
+echo -e "========================================================================="
+echo -e ""
